bb1f9ac2e201e819cbdff196da0044903d6fc6c6
Elijah Rutschman <elijahr@gmail.com>
2011-12-31 12:22:58 -0600

    cleanup handler

--- themailserver/app/handlers/sample.py
+++ themailserver/app/handlers/sample.py
@@ -1,32 +1,47 @@
-import logging
+
 from lamson.routing import route, route_like, stateless
 from config.settings import relay
 from lamson import view
 
-# for now this only accepts ASCII. must look into unicode ranges in regular expressions
+import logging
+
+# TODO:
+# - for now this only accepts ASCII. must look into unicode ranges in regular expressions
+# - create a proper FSM state for handling the clipboard
+
 @route("(badformat)@(host)", badformat=".+")
 @route("(address)@(host)", address="[a-zA-Z0-9\._]+")
 @route("(address)\-(signal)@(host)", address="[a-zA-Z0-9\._]+", signal="[a-zA-Z0-9\._]+")
 def START(message, address=None, signal=None, host=None, badformat=None):
-    if badformat:
-        logging.debug('Address "%s" is ambiguous. Addresses should be of the form user@example.com or user-signal@example.com where user and signal can only consist of characters in the set [a-zA-Z0-9], a period, or an underscore.' % (badformat))
-    elif signal:
-        logging.debug('Signal "%s" sent to %s' % (signal, address))
-        if signal == 'clipboard':
-            clipboard(message, address, host)
-    else:
-        logging.debug('Message sent to %s' % (address))
-    logging.debug('Message is:\n"""\n%s\n"""' % message.body())
+    """
+    This is the main point of entry for all incoming mail
+    """
 
-def clipboard(message, address, host):
+    if badformat:
+        logging.debug('Address "%s" is ambiguous. Addresses should be of the form user@example.com or user-signal@example.com where user and signal can only consist of characters in the set [a-zA-Z0-9], a period, or an underscore.' % badformat)
+
+    elif signal:
+        logging.debug('Signal "%s" received for %s' % (signal, address))
+
+        # handle signals here
+        if signal == 'clipboard':
+            handle_clipboard(message, address, host)
+
+        else:
+            logging.debug('Nothing to do for signal "%s" for %s' % (signal, address))
+
+    else:
+        logging.debug('Normal message sent to %s' % address)
+
+    #logging.debug('Message is:\n"""\n%s\n"""' % message.body())
+
+
+def handle_clipboard(message, address, host):
     i = 0
     for attachment in message.walk():
-        # the first attachment is actually just the message body
         if i > 0:
-            try:
-                logging.debug('Message contains attachment %s' % attachment.content_encoding)
-            except:
-                logging.debug('Message contains attachment with unknown encoding')
+            # the first attachment is actually just the message body so we skip it
 
-        # do some stuff with the file here, save it, add a DB record, etc
+            logging.debug('Message contains attachment %s' % attachment.content_encoding)
+            # do_stuff_here()
         i += 1
